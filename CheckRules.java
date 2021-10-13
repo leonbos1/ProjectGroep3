5467b206c2b5e423e2b9ee75956813096a1731a1
leonbos1 <76918287+leonbos1@users.noreply.github.com>
2021-10-13 18:16:08 +0200

    accepts row en col

--- CheckRules.java
+++ CheckRules.java
@@ -1,16 +1,12 @@
 public class CheckRules {
 
-    public static boolean checkLegalMove(int[][] board, int coordinate) {
-        int maxcoord = board[0].length*board.length;
+    public static boolean checkLegalMove(int[][] board, int row, int col) {
 
-        if (coordinate > maxcoord || coordinate < 1) {
+        if (row >= board.length || col >= board[0].length) {
             return false;
         }
 
-        int row = coordinate/board[0].length;
-        int col = coordinate%board[0].length-1;
-
-        if (board[row][col]==0) {
+        else if (board[row][col]==0) {
             return true;
         }
         return false;
@@ -52,13 +48,13 @@ public class CheckRules {
 
     public static void main(String[] args) {
         int[][] board = {{1,-1,-1},
-                         {1,0,0},
-                         {1,1,1}};
+                         {1,1,0},
+                         {1,0,1}};
 
         int coordinate = 1;
 
-        //System.out.println(checkLegalMove(board,coordinate));
-        System.out.println(checkWinner(board,1));
+        System.out.println(checkLegalMove(board,2,1));
+        //System.out.println(checkWinner(board,1));
 
     }
 
