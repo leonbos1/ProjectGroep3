009663f505413293e4a2da188e55983e2cab7ecc
leonbos1 <76918287+leonbos1@users.noreply.github.com>
2021-11-14 21:21:55 +0100

    template code javaFX

---
+++ Window.java
@@ -0,0 +1,35 @@
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+// download link https://download2.gluonhq.com/openjfx/17.0.1/openjfx-17.0.1_windows-x64_bin-sdk.zip
+
+
+public class Window extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Hello World!");
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Hello World!");
+            }
+        });
+
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        primaryStage.setScene(new Scene(root, 300, 250));
+        primaryStage.show();
+    }
+}
--- main.java
+++ main.java
@@ -2,6 +2,7 @@ import java.lang.*;
 import java.util.Arrays;
 import java.util.Scanner;
 import java.io.*;
+
 class main{
 
     public static void main(String[] args) {
