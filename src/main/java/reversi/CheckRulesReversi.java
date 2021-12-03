ce3c3d574ea4ccd9f4233b02610c39f687cb4c92
leonbos1 <76918287+leonbos1@users.noreply.github.com>
2021-12-03 11:39:26 +0100

    broken minimax

--- src/main/java/reversi/CheckRulesReversi.java
+++ src/main/java/reversi/CheckRulesReversi.java
@@ -320,7 +320,7 @@ public class CheckRulesReversi {
         return false;
     }
 
-    public int checkPlayerScore() {
+    public int checkPlayerScore(int player) {
         int playerScore = 0;
         for (int row = 0; row < 8; row++) {
             for (int col = 0; col < 8; col++) {
--- src/main/java/reversi/ReversiAI.java
+++ src/main/java/reversi/ReversiAI.java
@@ -4,7 +4,20 @@ import src.main.java.tictactoe.CheckRules;
 
 import java.util.Random;
 
+
+
 public class ReversiAI {
+
+    private static final int[][] pointsBoard = new int[][] {
+        {200 , -100, 100, 50 , 50 , 100, -100,  200},
+        {-100, -200, -50, -50, -50, -50, -200, -100},
+        {100 , -50 , 100, 0  , 0  , 100, -50 ,  100},
+        {50  , -50 , 0  , 0  , 0  , 0  , -50 ,  50},
+        {50  , -50 , 0  , 0  , 0  , 0  , -200, -100},
+        {100 , -50 , 100, 0  , 0  , 100, -50 , -100},
+        {-100, -200, -50, -50, -50, -50, -200, -100},
+        {200 , -100, 100, 50 , 50 , 100, -100, 200}
+    };
 
     Board board;
     CheckRulesReversi rules;
@@ -13,7 +26,6 @@ public class ReversiAI {
     public ReversiAI(Board board, int player) {
         this.board = board;
         this.player = player;
-        rules = new CheckRulesReversi(board, player);
 
     }
 
@@ -33,45 +45,94 @@ public class ReversiAI {
         return new int[]{row, col};
     }
 
-    public int[] AIMove(int player) {
-        int maxRow = 0;
-        int maxCol = 0;
-        int maxScore= -500;
-        int[][] points = new int[8][8];
-
-        int[][] pointsBoard = new int[][] {
-                {200 , -100, 100, 50 , 50 , 100, -100,  200},
-                {-100, -200, -50, -50, -50, -50, -200, -100},
-                {100 , -50 , 100, 0  , 0  , 100, -50 ,  100},
-                {50  , -50 , 0  , 0  , 0  , 0  , -50 ,  50},
-                {50  , -50 , 0  , 0  , 0  , 0  , -200, -100},
-                {100 , -50 , 100, 0  , 0  , 100, -50 , -100},
-                {-100, -200, -50, -50, -50, -50, -200, -100},
-                {200 , -100, 100, 50 , 50 , 100, -100, 200}
-        };
+    public int[] AIMove(Reversi reversi, int player, int depth) {
+        Reversi reversiCopy = new Reversi(player);
 
         for (int row = 0; row <= 7; row++) {
             for (int col = 0; col <= 7; col++) {
-                if (rules.checkLegalMove(row,col,player)) {
-                    points[row][col] = pointsBoard[row][col];
-                }
-                else {points[row][col] = -1000;}
+                reversiCopy.board.board[row][col] = reversi.getBoardArray()[row][col];
             }
         }
 
+        int[][] points = pointsForEachMove(reversiCopy, player, depth);
+
+        int maxscore = -10000;
+        int maxRow = 0;
+        int maxCol = 0;
+
         for (int row = 0; row <= 7; row++) {
             for (int col = 0; col <= 7; col++) {
-                if (points[row][col] > maxScore) {
-                    maxScore = points[row][col];
+                if (maxscore < points[row][col]) {
+                    maxscore = points[row][col];
                     maxRow = row;
                     maxCol = col;
                 }
             }
         }
-
+        System.out.println("een zet");
         return new int[]{maxRow,maxCol};
+
     }
 
+    public int [][] pointsForEachMove(Reversi reversi, int player, int depth) {
+        int[][] points = new int[8][8];
+        int maxScore = -10000;
 
+        rules = new CheckRulesReversi(reversi.board, player);
+
+        for (int row = 0; row <= 7; row++) {
+            for (int col = 0; col <= 7; col++) {
+                if (!rules.checkLegalMove(row,col, player)) {
+                    points[row][col] = -1000;
+                }
+
+                else if (rules.gameOver() && (rules.checkPlayerScore(player) > rules.checkPlayerScore(Reversi.getOpponent(player))) ) {
+                    points[row][col] = 1000;
+                }
+
+                else if (rules.gameOver() && (rules.checkPlayerScore(player) < rules.checkPlayerScore(Reversi.getOpponent(player))) ) {
+                    points[row][col] = -500;
+                }
+
+                else if (depth == 0) {
+                    points[row][col] = pointsBoard[row][col];
+                    for (int i = 0; i <= 7; i++) {
+                        for (int j = 0; j <= 7; j++) {
+                            System.out.println(points[i][j]);
+                        }
+                    }
+
+                }
+
+                else {
+                    if (rules.checkLegalMove(row,col,player)) {
+                        reversi.makeMove(player, row, col);
+
+
+                        for (int i = 0; i <= 7; i++) {
+                            for (int j = 0; j <= 7; j++) {
+                                if (rules.checkLegalMove(Reversi.getOpponent(player), i, j)) {
+                                    points[i][j] = pointsBoard[i][j];
+                                    if (maxScore < points[i][j]) {
+                                        maxScore = points[i][j];
+                                        row = i;
+                                        col = j;
+                                    }
+                                }
+                            }
+                        }
+                        reversi.makeMove(Reversi.getOpponent(player),row,col);
+
+
+                        pointsForEachMove(reversi, player, depth-1);
+                    }
+
+                }
+
+            }
+        }
+
+        return points;
+    }
 
 }
